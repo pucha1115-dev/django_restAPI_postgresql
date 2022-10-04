4c01a16326010d7324589137d5896d10804be0ba
jp-bns2020 <71251589+pucha1115-dev@users.noreply.github.com>
2022-10-05 00:25:49 +0800

    Add files via upload

---
+++ DjangoAPI/EmployeeApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import re_path as url 
+from EmployeeApp import views
+
+urlpatterns = [
+    url(r'^department$', views.departmentApi),
+    url(r'^department/([0-9]+)$', views.departmentApi),
+]
